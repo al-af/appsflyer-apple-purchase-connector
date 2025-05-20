d5d67bb10e085242d3776ff62e927dc05f8e2c8b
Amit Levy <amit.levy@appsflyer.com>
2025-05-20 16:29:36 +0300

    update readme.sh

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Usage: scripts/update_readme.sh <version>
 VERSION="$1"
 README="README.md"
 
-# 1) Update the “built for” table header line
+# 1) Update the bolded SDK version in the "Built for" section
+#    Matches: iOS AppsFlyer SDK **x.y.z**
 sed -i.bak -E \
-  "s|(\|[[:space:]]*iOS[[:space:]]+AppsFlyer SDK )[0-9]+\.[0-9]+\.[0-9]+|\1${VERSION}|" \
+  "s|(iOS AppsFlyer SDK \*\*)[0-9]+\.[0-9]+\.[0-9]+(\*\*)|\1${VERSION}\2|" \
   "$README"
 
-# 2) Append a new row under that table’s header 
-sed -i.bak -E "/\|[[:space:]]*Platform[[:space:]]*\|[[:space:]]*SDK Version/ a \
-| iOS | AppsFlyer SDK ${VERSION} |" \
-  "$README"
-
-# 3) Update the Carthage snippet version in the “Adding The Connector…” section
-sed -i.bak -E \
-  "s|(github\.com/.*/releases/download/)[0-9]+\.[0-9]+\.[0-9]+(/purchase-connector-)(dynamic|static)\.xcframework\.zip|\1${VERSION}\2\3.xcframework.zip|" \
-  "$README"
-
-# cleanup backup
+# Cleanup backup file
 rm "${README}.bak"
